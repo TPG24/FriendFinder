b1288423811acb273e6c41b98a904547844ed5a6
Your Name <tonygiamboy@yahoo.com>
2018-05-10 14:53:36 -0400

    friends array layout set

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+let friendsArray = [(
+    {
+        name: "",
+        photo: "",
+        scores: []
+    }, {
+        name: "",
+        photo: "",
+        scores: []
+    },  {
+        name: "",
+        photo: "",
+        scores: []
+    },  {
+        name: "",
+        photo: "",
+        scores: []
+    },  {
+        name: "",
+        photo: "",
+        scores: []
+    }
+)];
+
+module.exports = friendsArray;
