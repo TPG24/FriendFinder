9828dc99e87d51c16289e66ef106926c6d5f64dc
Your Name <tonygiamboy@yahoo.com>
2018-05-10 13:07:24 -0400

    layout built and the htmlRoutes page created

---
+++ server.js
@@ -0,0 +1,33 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+let PORT = process.env.PORT || 8080;
+
+
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+});
+
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(function (req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('you posted:\n');
+    res.end(JSON.stringify(req.body, null, 2))
+});
+
+
+
+
+app.listen(PORT, function(){
+    console.log('App listening on PORT ' + PORT);
+});
