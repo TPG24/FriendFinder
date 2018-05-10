d281e49eab6d6a58e8106012c9211a5bf70de2b1
Your Name <tonygiamboy@yahoo.com>
2018-05-10 15:08:41 -0400

    fixed bug on loading port

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ let PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-});
+// app.get('/', function (req, res) {
+//     res.send('Hello World')
+// });
 
 
 
@@ -19,11 +19,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(function (req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('you posted:\n');
-    res.end(JSON.stringify(req.body, null, 2))
-});
+// app.use(function (req, res) {
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.write('you posted:\n');
+//     res.end(JSON.stringify(req.body, null, 2))
+// });
+
+require('./app/routing/htmlRoutes.js')(app);
+// require('./app/routing/apiRoutes.js')(app);
 
 
 
